ce2ea8fdae747f70725cc569ee232cad0ec2f629
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 01:58:15 -0300

    chore: jest

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+    coverageDirectory: "coverage",
+    coverageProject: "v8",
+    coverageReporters: ["text", "lcov"],
+    collectCoverageFrom: ["src/**/*.js", "!src/errors/*.js", "!src/interface/*.js"],
+    coverageThreshold: {
+      global: {
+        branch: 100,
+        functions: 100,
+        lines: 100,
+        statements: 100,
+      },
+    },
+    maxWorkers: "50%",
+    testEnvironment: "node",
+    watchPathIgnorePatterns: ["node_modules"],
+    transformIgnorePatterns: ["node_modules"],
+  };
