8c978ef9e8894836820a0be1461b95d07cdbb75f
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 03:06:49 -0300

    chore: minimal changes to test the youtube music entity

--- src/modules/youtube/dto/youtubeMusicDTO.js
+++ src/modules/youtube/dto/youtubeMusicDTO.js
@@ -1,5 +1,8 @@
 export default class YoutubeMusicDTO {
     constructor({ track }) {
         this.artist_name = track.artists.artist_name;
+        this.album_name = track.album.name;
+        this.music_name = track.name;
+        this.duration = track.time_ms;
     }
 }
