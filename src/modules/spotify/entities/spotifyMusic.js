255be7935d33b53054a2b393fa79044ea97f5dd8
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 02:54:32 -0300

    feat: spotify music entity

--- src/modules/spotify/entities/spotifyMusic.js
+++ src/modules/spotify/entities/spotifyMusic.js
@@ -4,4 +4,12 @@ export default class SpotifyMusic extends Music {
     constructor({...musicProps}) {
         super((musicProps));
     }
+
+    toString() {
+        return `${this.music_name} - ${this.album_name} - ${this.artist_name}`
+    }
+
+    valueOf() {
+        return this.duration;
+    }
 }
