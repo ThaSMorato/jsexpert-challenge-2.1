11c322d16a6081115ca49f106e056a021eea3083
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 01:23:17 -0300

    refact: fake api url

--- src/util/api.js
+++ src/util/api.js
@@ -1,6 +1,6 @@
 export default class Api {
     static async get(param) {
-        const jsonResponse = await fetch(`url/${param}`);
+        const jsonResponse = await fetch(`http://localhost:3001/${param}`);
         return await jsonResponse.json();
     }
 }
