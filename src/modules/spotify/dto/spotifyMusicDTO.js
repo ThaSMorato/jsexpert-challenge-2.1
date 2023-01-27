9bd0782d66229c764d3fc1b808e9287531015ffa
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 02:53:15 -0300

    chore: minimal changes to test spotify music entity

--- src/modules/spotify/dto/spotifyMusicDTO.js
+++ src/modules/spotify/dto/spotifyMusicDTO.js
@@ -1,5 +1,8 @@
 export default class SpotifyMusicDTO {
     constructor({ track }) {
         this.artist_name = track.artists.name;
+        this.album_name = track.album.name;
+        this.music_name = track.name;
+        this.duration = track.duration_ms;
     }
 }
