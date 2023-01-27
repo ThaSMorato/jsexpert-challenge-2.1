4d9150788408c205964a27f27b2a8c38a0ce2715
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 02:53:52 -0300

    test: more precisily data

--- test/mocks/spotifyApiMock.js
+++ test/mocks/spotifyApiMock.js
@@ -1,4 +1,5 @@
 import { jest } from "@jest/globals";
+import SpotifyMusicDTO from "../../src/modules/spotify/dto/spotifyMusicDTO";
 
 
 export const spotifyApiMock = {
@@ -10,23 +11,34 @@ export const mock = [
         track:{
             artists: {
                 name: "a"
-            }
+            },
+            album: { name: "aa"},
+            name: "aaa",
+            duration_ms: 156156,
         }
     },
     {
         track:{
             artists: {
                 name: "b"
-            }
+            },
+            album: { name: "bb"},
+            name: "bbb",
+            duration_ms: 156156,
         }
     },
     {
         track:{
             artists: {
                 name: "c"
-            }
+            },
+            album: { name: "cc"},
+            name: "ccc",
+            duration_ms: 156156,
         }
     },
 ]
 
 spotifyApiMock.get.mockResolvedValue(mock);
+
+export const transformedMusic = new SpotifyMusicDTO(mock[0]);
