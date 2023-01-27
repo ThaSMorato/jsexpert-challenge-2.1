59fe6e83c953c11082bde83cb6503088656cd5a2
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 02:01:16 -0300

    test: spotify repository

---
+++ test/unit/modules/repositories/SpotifyRepository.test.js
@@ -0,0 +1,31 @@
+import { beforeAll, describe, expect, it, jest } from '@jest/globals'
+import SpotifyRepository from '../../../../src/modules/spotify/repositories/SpotifyRepository';
+import SpotifyMusic from '../../../../src/modules/spotify/entities/spotifyMusic';
+import { spotifyApiMock, mock } from '../../../mocks/spotifyApiMock';
+
+describe('#SpotifyRepository', () => {
+    let repository;
+
+    beforeAll(() => {
+        repository = new SpotifyRepository({api: spotifyApiMock})
+    })
+
+    describe(".getAllMusics", () => {
+        it('should call get on api with the string spotify as param',async  () => {
+            await repository.getAllMusics();
+
+            expect(spotifyApiMock.get).toBeCalledTimes(1);
+            expect(spotifyApiMock.get).toBeCalledWith('spotify');
+        });
+
+        it('should return an array of SpotifyMusic from the api', async () => {
+
+            const result = await repository.getAllMusics();
+
+            const expected = mock.map(music => new SpotifyMusic(music))
+
+            expect(result.join()).toBe(expected.join());
+            expect(result[0]).toBeInstanceOf(SpotifyMusic);
+        });
+    })
+});
