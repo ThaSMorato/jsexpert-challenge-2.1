18d1bf13db633657511c06bd0fe8e4e53cc3ca85
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 03:07:14 -0300

    test: more realistic mock

--- test/mocks/youtubeApiMock.js
+++ test/mocks/youtubeApiMock.js
@@ -1,4 +1,5 @@
 import { jest } from "@jest/globals";
+import YoutubeMusicDTO from "../../src/modules/youtube/dto/youtubeMusicDTO";
 
 
 export const youtubeApiMock = {
@@ -10,23 +11,34 @@ export const mock = [
         track:{
             artists: {
                 artist_name: "a"
-            }
+            },
+            album: {name: "aa"},
+            name: "aaa",
+            time_ms: 545488,
         }
     },
     {
         track:{
             artists: {
                 artist_name: "b"
-            }
+            },
+            album: {name: "bb"},
+            name: "bbb",
+            time_ms: 545488,
         }
     },
     {
         track:{
             artists: {
                 artist_name: "c"
-            }
+            },
+            album: {name: "cc"},
+            name: "ccc",
+            time_ms: 545488,
         }
     },
 ]
 
 youtubeApiMock.get.mockResolvedValue(mock);
+
+export const transformedMusic = new YoutubeMusicDTO(mock[0]);
