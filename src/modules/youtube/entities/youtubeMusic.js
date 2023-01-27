4821095bfe60c89ba13a15a958c8cd469de420dd
ThaSMorato <tha.s.morato@gmail.com>
2023-01-27 03:07:41 -0300

    feat: youtubr music entity

--- src/modules/youtube/entities/youtubeMusic.js
+++ src/modules/youtube/entities/youtubeMusic.js
@@ -4,4 +4,10 @@ export default class YoutubeMusic extends Music {
     constructor({...musicProps}) {
         super((musicProps));
     }
+
+    [Symbol.toPrimitive](coercionType){
+        return coercionType === "string"
+          ? `${this.music_name} - ${this.album_name} - ${this.artist_name}`
+          : this.duration
+    }
 }
