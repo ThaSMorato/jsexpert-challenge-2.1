51496cbc5817d04c961a5ac1cbc0a965c534a9f7
ThaSMorato <tha.s.morato@gmail.com>
2023-02-01 03:02:28 -0300

    test: playlist interface

---
+++ test/unit/interface/playlist.test.js
@@ -0,0 +1,29 @@
+import { describe, expect, it, jest } from '@jest/globals'
+import Playlist from '../../../src/interface/playlist'
+import SpotifyMusic from '../../../src/modules/spotify/entities/spotifyMusic'
+import formatMilisseconds from '../../../src/util/timeFormat'
+import { transformedMusic } from '../../mocks/spotifyApiMock'
+
+describe("#Playlist", () => {
+  describe(".toResponse", () => {
+    it("should return the playlist already formatted", () => {
+
+      const music = new SpotifyMusic(transformedMusic)
+      const musics = [music, music];
+
+      const playlist = new Playlist({ musics });
+
+      const resp = playlist.toResponse();
+
+      const expected = {
+        musics: [
+          { display: `${music}`, duration: formatMilisseconds(+music) },
+          { display: `${music}`, duration: formatMilisseconds(+music) }
+        ],
+        duration: formatMilisseconds(music + music)
+      }
+
+      expect(resp).toEqual(expected);
+    })
+  })
+})
