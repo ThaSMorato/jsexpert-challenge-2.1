70c8ac0650dee09552ed0a57ef21713e1c11725b
ThaSMorato <tha.s.morato@gmail.com>
2023-02-01 03:13:47 -0300

    feat: playlist use case

--- src/useCase/playlistUseCase.js
+++ src/useCase/playlistUseCase.js
@@ -1,6 +1,16 @@
+import Playlist from "../interface/playlist";
+
 class PlaylistUseCase {
   constructor({ repository }) {
     this.repository = repository;
+  }
+
+  async perform() {
+    const musics = await this.repository.getAllMusics();
+
+    const playlist = new Playlist({ musics });
+
+    return playlist.toResponse();
   }
 }
 
