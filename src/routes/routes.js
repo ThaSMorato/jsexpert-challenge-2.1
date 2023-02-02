cafa982c00cd8fc15b0279247672622373e0fae7
ThaSMorato <tha.s.morato@gmail.com>
2023-02-02 02:05:09 -0300

    refact: bind

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,4 @@
-import ControllerFactory from "../factory/controllerFactory";
+import ControllerFactory from "../factory/controllerFactory.js";
 
 export class Routes {
   handler(request, response) {
@@ -12,14 +12,16 @@ export class Routes {
   }
 
   createRoutes() {
+    const youtubeController = ControllerFactory.getYoutubeInstance()
+    const spotifyController = ControllerFactory.getSpotifyInstance()
     return {
       default: (request, response) => {
         response.writeHeader(200, { "Content-Type": "text/html" });
         response.write("Hey there, try /youtube or /spotify to see my playlists");
         return response.end();
       },
-      "/youtube:get": ControllerFactory.getYoutubeInstance().handle,
-      "/spotify:get": ControllerFactory.getSpotifyInstance().handle,
+      "/youtube:get": youtubeController.handle.bind(youtubeController),
+      "/spotify:get": spotifyController.handle.bind(spotifyController),
     };
   }
 }
