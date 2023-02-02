744ef0d941aa7518f9d6a89706809d5cab02709a
ThaSMorato <tha.s.morato@gmail.com>
2023-02-02 02:27:51 -0300

    chore: super test and cov

--- jest.config.js
+++ jest.config.js
@@ -4,20 +4,20 @@
  */
 
 export default {
-    coverageDirectory: "coverage",
-    coverageProject: "v8",
-    coverageReporters: ["text", "lcov"],
-    collectCoverageFrom: ["src/**/*.js", "!src/errors/*.js", "!src/interface/*.js"],
-    coverageThreshold: {
-      global: {
-        branch: 100,
-        functions: 100,
-        lines: 100,
-        statements: 100,
-      },
+  coverageDirectory: "coverage",
+  coverageProject: "v8",
+  coverageReporters: ["text", "lcov"],
+  collectCoverageFrom: ["src/**/*.js", "!src/index.js"],
+  coverageThreshold: {
+    global: {
+      branch: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
-    maxWorkers: "50%",
-    testEnvironment: "node",
-    watchPathIgnorePatterns: ["node_modules"],
-    transformIgnorePatterns: ["node_modules"],
-  };
+  },
+  maxWorkers: "50%",
+  testEnvironment: "node",
+  watchPathIgnorePatterns: ["node_modules"],
+  transformIgnorePatterns: ["node_modules"],
+};
